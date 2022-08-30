ccdb452e10eb1d96abebfcae163ad42d569cf26d
LucasPCotrim <lucaspcotrim@gmail.com>
2022-08-30 00:07:44 -0300

    feat: Implemented initial connection to mongodb and database creation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
+// Configure Dotenv
+dotenv.config();
+
+// Create server
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log(`Server listening on port: 5000`);
+// Connect to MongoDB
+let db = null;
+const mongoClient = new MongoClient(process.env.MONGO_DATABASE_URI);
+const promise = mongoClient.connect().then(() => {
+  db = mongoClient.db(process.env.MONGO_DATABASE_NAME);
+});
+promise.catch((err) => {
+  console.log('Error when trying to connect to the database:', err);
+});
+
+// Initialize Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
 });
